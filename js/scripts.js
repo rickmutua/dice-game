509ec55892e48f3439906c2a4759e73a78fef3ba
rickmutua <rick.mutua12@gmail.com>
2017-06-16 15:42:12 +0300

    updated user logic and html

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
     player1 = new Player(true);
     player2 = new Player(false);
 
-    $(".player-console").show();
+    $(".player-gaming").show();
     $(".start-menu").hide();
 
     var player1Name = $(".player1Name").val();
@@ -69,17 +69,57 @@ $(document).ready(function() {
   });
 
   $("button#new-game").click(function(event) {
-    $(".player-console").hide();
+    $(".player-gaming").hide();
       clearValues();
         player1.newGame();
         player2.newGame();
+        $("#die-toss-1").empty();
 
     $("#round-total-1").empty();
      $("#total-score-1").empty();
-      $("#die-toss-1").empty();
        $("#round-total-2").empty();
         $("#total-score-2").empty();
          $("#die-toss-2").empty();
 
     $(".start-menu").show();
   });
+
+  $("button#player1-toss").click(function(event) {
+    player1.toss = throwdice();
+
+      $("#die-toss-1").text(player1.toss);
+        player1.tossone();
+
+      $("#round-total-1").text(player1.tempscore);
+    });
+
+    $("button#player2-toss").click(function(event) {
+      player2.roll = throwdice();
+
+      $("#die-toss-2").text(player2.toss);
+        player2.tossone();
+
+      $("#round-total-2").text(player2.tempscore);
+    });
+
+  $("button#player1-hold").click(function(event) {
+    player1.hold();
+
+   $("#total-score-1").text(player1.totalscore);
+    $("#round-total-1").empty();
+     $("#die-toss-1").empty();
+
+   player1.winnerCheck();
+ });
+
+ $("button#player2-hold").click(function(event) {
+   player2.hold();
+
+   $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+     $("#die-toss-2").empty();
+
+   player2.winnerCheck();
+ });
+
+});
